58138e4925046d7d6cf721e792013fa1ee0766b4
LucsT <lucas@cozycloud.cc>
2020-10-29 10:49:50 +0100

    feat: First version with login, scraping of files and directories

--- src/index.js
+++ src/index.js
@@ -3,122 +3,103 @@ const {
   requestFactory,
   scrape,
   log,
-  utils
+  utils,
+  saveFiles,
+  errors
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: false,
-  // This allows request-promise to keep cookies between requests
+  debug: true,
+  cheerio: false,
+  json: false, // Despite the wide use of json in the api, we need querystring params too
   jar: true
 })
 
-const VENDOR = 'template'
-const baseUrl = 'http://books.toscrape.com'
+const VENDOR = 'edoc'
+const appDomain = 'https://app.edocperso.fr'
+const appUrl = appDomain + '/api/index.php'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
 async function start(fields, cozyParameters) {
   log('info', 'Authenticating ...')
   if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
-  await authenticate.bind(this)(fields.login, fields.password)
+  const sessionId = await authenticate.bind(this)(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
+  console.log(sessionId)
+
   log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
+  const documentsTree = await parseDocuments(sessionId)
+  console.log(JSON.stringify(documentsTree,null,2))
+  // Recursively call this function on the tree of files and directories
+  const files = extractFilesAndDirs(documentsTree, '', sessionId)
+  console.log(files)
 
-  // Here we use the saveBills function even if what we fetch are not bills,
-  // but this is the most common case in connectors
   log('info', 'Saving data to Cozy')
-  await this.saveBills(documents, fields, {
-    // This is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
+  await saveFiles(files,
+                  fields,
+                 )
 }
 
-// This shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return this.signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // The validate function will check if the login request was a success. Every website has a
-    // different way to respond: HTTP status code, error message in HTML ($), HTTP redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be useful for other connectors'
-      )
-      // The login in toscrape.com always works except when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
+async function authenticate(username, password) {
+  const rLogin = await request({uri: appUrl,
+                     method: 'POST',
+                     qs : {api : 'Authenticate', a : 'doAuthentication'},
+                     json: {login: username, password}
+                    })
+  if (rLogin.status && rLogin.status == 'success') {
+    const sessionId = rLogin.content.loginUrl.split('\/').pop()
+    return sessionId
+  } else if (rLogin.status && rLogin.status == 'error' &&
+             rLogin.code && rLogin.code == 4) {
+    log('error', rLogin)
+    throw new Error(errors.LOGIN_FAILED)
+  } else {
+    log('error', 'Get an unexpected result during login')
+    log('error', rLogin)
+    throw new Error(errors.VENDOR_DOWN)
+  }
+}
+
+async function parseDocuments(sessionId) {
+  const rDocs = await request({uri: appUrl,
+                               method: 'POST',
+                               qs : {api : 'User', a : 'getFoldersAndFiles'},
+                               json: {sessionId}
+                              })
+  if (!rDocs.code && rDocs.code != 0) {
+    log('error', 'Get an unexpected result during documents listing')
+    log('error', 'rDocs')
+    throw new Error(errors.VENDOR_DOWN)
+  }
+  return rDocs.content
+}
+
+function extractFilesAndDirs(docsTree, currentPath, sessionId) {
+  let newArray = []
+  console.log('Array '+newArray)
+  for (const doc of docsTree) {
+    if (doc.type == 'folder') {
+      console.log('is Folder')
+      const newPath = currentPath + '/' + doc.name
+      newArray = newArray.concat(extractFilesAndDirs(doc.children, newPath, sessionId))
+    } else if (doc.type == 'file') {
+      console.log('is File')
+      console.log(doc)
+      const file = appendFileData(doc, currentPath, sessionId)
+      newArray.push(file)
     }
-  })
+  }
+  return newArray
 }
 
-// The goal of this function is to parse a HTML page wrapped by a cheerio instance
-// and return an array of JS objects which will be saved to the cozy by saveBills
-// (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // You can find documentation about the scrape function here:
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // The saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: 'EUR',
-    filename: `${utils.formatDate(new Date())}_${VENDOR}_${doc.amount.toFixed(
-      2
-    )}EUR${doc.vendorRef ? '_' + doc.vendorRef : ''}.jpg`,
-    vendor: VENDOR
-  }))
-}
-
-// Convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+function appendFileData(doc, currentPath, sessionId) {
+  console.log('Session' +sessionId)
+  return {
+    filename: doc.name,
+    fileurl: `${appUrl}?api=UserDocument&a=getContentAsGet&sessionId=${sessionId}&documentId=${doc.id}&download=1`,
+    subPath: currentPath,
+    id : doc.id,
+    date: doc.depositDate,
+    issuerName: doc.issuerName
+  }
 }
